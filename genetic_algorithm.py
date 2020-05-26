af9403c4006b869d190899bb231f54d0b6e7d17b
Grzegorz Krug <grzesiekk321@gmail.com>
2020-05-26 03:40:10 +0200

    plotig houses

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -8,6 +8,7 @@ import datetime
 import numpy as np
 
 BOARD_SIZE = 27
+HOME_FIX_OVERWRITE = True
 
 
 class Game:
@@ -16,23 +17,65 @@ class Game:
         self.score = 0
         self.random_board()
 
-    def draw(self):
+    def draw(self, debug=False):
         plt.figure(figsize=(8, 8))
         for y, row in enumerate(self.board):
             for x, element in enumerate(row):
+                if debug:
+                    plt.text(x, y, "o", fontsize=10)
                 if type(element) is Home:
-                    fontsize = 50
+                    if element.base:
+                        plt.text(x - 0.2, y + 0.2, element.marker, fontsize=element.size, color=element.color)
                 else:
                     fontsize = 20
-                plt.text(x, y, element.marker, fontsize=fontsize, color=element.color)
-
+                    plt.text(x, y, element.marker, fontsize=element.size, color=element.color)
+        if debug:
+            dist = 27
+            plt.plot([0, dist, dist, 0, 0], [0, 0, dist, dist, 0], 'k', '.-')
         plt.xlim([-1, 28])
         plt.ylim([-1, 28])
         plt.grid()
         plt.show()
 
+    @staticmethod
+    def house_fields(x, y):
+        yield x, y + 1
+        yield x + 1, y
+        yield x + 1, y + 1
+
     def validate(self):
-        pass
+        for y, row in enumerate(self.board):
+            for x, element in enumerate(row):
+                if type(element) is Home:
+                    if element.base:
+                        try:
+                            for field in self.house_fields(y, x):
+                                assert type(self.board[field]) is Home and self.board[field].base is False
+                            valid = True
+                        except (KeyError, AssertionError):
+                            valid = False
+                        except IndexError:
+                            self.board[y, x] = Road()
+                            continue
+
+                        if not valid and HOME_FIX_OVERWRITE:
+                            try:
+                                for field in self.house_fields(y, x):
+                                    self.board[field] = Home()
+                            except IndexError:
+                                self.board[y, x] = Road()
+
+                        elif not valid:
+                            self.board[y, x] = Road()
+
+                elif type(element) is Road:
+                    pass
+                elif type(element) is Tower:
+                    pass
+                elif type(element) is Figure:
+                    raise ValueError("How did you get here? Base class figure is on board!")
+                else:
+                    raise ValueError(f"This object has unknown type: {type(element)}")
 
     def random_board(self):
         self.board = np.zeros((BOARD_SIZE, BOARD_SIZE), dtype=Figure)
@@ -46,14 +89,24 @@ class Game:
                 while roll > chance:
                     roll -= chance
                     selection += 1
-                self.board[x, y] = pool[selection]()
+
+                figure = pool[selection]()
+                if type(figure) is Home:
+                    figure.base = True
+                self.board[x, y] = figure
+
+        self.board[0, 0] = Home(base=True)
+        self.board[0, 1] = Home(base=True)
+        self.board[0, 2] = Home(base=True)
+        self.board[0, 3] = Home(base=True)
 
 
 class Figure:
     def __init__(self):
         super()
-        self.marker = '.'
-        self.color = 'w'
+        self.marker = ''
+        self.color = 'k'
+        self.size = 20
 
     def __repr__(self):
         return "F"
@@ -70,13 +123,16 @@ class Road(Figure):
 
 
 class Home(Figure):
-    def __init__(self):
+    def __init__(self, base=False):
         super().__init__()
         self.power = False
         self.path = False
         self.position = None
         self.marker = '⌂'
+        self.marker = '▩'
+        self.size = 37
         self.color = 'g'
+        self.base = base
 
     def __repr__(self):
         return "H"
@@ -104,4 +160,5 @@ class Bank(Figure):
 
 if __name__ == "__main__":
     game = Game()
+    game.validate()
     game.draw()
